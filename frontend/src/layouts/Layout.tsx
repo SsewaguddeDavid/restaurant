2e5919eefffbf984e92e69dabf3c419e43796c25
Ssewagudde David <dsewagude10@gmail.com>
2025-05-06 05:32:31 +0300

    add user Profile form

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -3,11 +3,15 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+type Props = {
+  children: React.ReactNode;
+  showHero?: boolean;
+};
+const Layout = ({ children, showHero = false }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
