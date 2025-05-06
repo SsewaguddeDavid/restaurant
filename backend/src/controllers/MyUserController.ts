0e28dccd50dbf60c5f283afe1170517d5b18c618
Ssewagudde David <dsewagude10@gmail.com>
2025-05-06 04:37:10 +0300

    add update user profile route and middleware

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,32 +1,57 @@
 import { Request, Response } from "express";
 import User from "../models/user"; // Make sure the model is correctly imported
 
-const MyUserController = {
-  createCurrentUser: async (req: Request, res: Response) => {
-    try {
-      const { auth0Id } = req.body;
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0Id } = req.body;
 
-      // Check if user already exists
-      const existingUser = await User.findOne({ auth0Id });
+    // Check if user already exists
+    const existingUser = await User.findOne({ auth0Id });
 
-      if (existingUser) {
-        res.status(200).send();
-        return;
-      }
-
-      // Create a new user
-      const newUser = new User(req.body);
-      await newUser.save();
-
-      // Return the newly created user
-      res.status(201).json(newUser.toObject());
-    } catch (error) {
-      console.error("Error creating user:", error);
-      res
-        .status(500)
-        .json({ error: "An error occurred while creating the user" });
+    if (existingUser) {
+      res.status(200).send();
+      return;
     }
-  },
+
+    // Create a new user
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    // Return the newly created user
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the user" });
+  }
 };
 
-export default MyUserController;
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
+export default {
+  createCurrentUser,
+  updateCurrentUser,
+};
